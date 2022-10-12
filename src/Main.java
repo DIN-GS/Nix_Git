eeea41461e14d7350fbc5a7378970ae3bb307079
DIN-GS <nikita.ivakin@nure.ua>
2022-10-12 14:52:25 +0300

    say hi to Nikita

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
 
         int r_number = (int)(Math.random() * 10 + 1);
         System.out.println("Your lucky number " + r_number);
-
+        System.out.println("New commit. Hi, Nikita. How are you?");
     }
 }
